76d93dab3e705151d0d9b6a206f1269719e78241
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-29 20:09:18 +0530

    searching element linear

--- searching_element_in_array_using_linear.cpp
+++ searching_element_in_array_using_linear.cpp
@@ -7,4 +7,13 @@ int main() {
     int data[MAX];
     int beg, end, mid, item, i, n;
 
+    cout << "Enter the size and elements of the array\n";
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+        cin >> data[i];
+
+    cout << "Following is the given array\n";
+    for (i = 0; i < n; i++)
+        cout << data[i] << "\t";
     
