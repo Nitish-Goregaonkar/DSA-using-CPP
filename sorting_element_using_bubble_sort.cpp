febfd61e7bbf7b0aaeb34ba3d24436862bc1a1c7
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-29 20:13:18 +0530

    sorting element using bubble sort

--- sorting_element_using_bubble_sort.cpp
+++ sorting_element_using_bubble_sort.cpp
@@ -4,4 +4,10 @@ using namespace std;
 #define MAX 100
 
 int main() {
+    int data[MAX];
+    int k, ptr, n, i, temp;
+
+    cout << "Enter the size and elements of the array\n";
+    cin >> n;
+
     
