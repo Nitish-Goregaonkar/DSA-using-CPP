2ae2e26cd5b5290eeb60354e4f910dcffde2b1c3
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-29 20:14:36 +0530

    sorting element using bubble sort

--- sorting_element_using_bubble_sort.cpp
+++ sorting_element_using_bubble_sort.cpp
@@ -10,4 +10,12 @@ int main() {
     cout << "Enter the size and elements of the array\n";
     cin >> n;
 
+    for (i = 0; i < n; i++)
+        cin >> data[i];
+
+    cout << "***** Array before sorting **********\n";
+    for (i = 0; i < n; i++)
+        cout << data[i] << "\t";
+    cout << "\n";
+
     
