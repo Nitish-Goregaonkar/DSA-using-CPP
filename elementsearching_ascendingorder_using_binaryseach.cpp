7205dce3b2a7f6f0721b8cbac6b398cab8c3a7a5
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-29 20:01:47 +0530

    element searching binarysearch

--- elementsearching_ascendingorder_using_binaryseach.cpp
+++ elementsearching_ascendingorder_using_binaryseach.cpp
@@ -10,4 +10,15 @@ int main() {
     cout << "Enter the size and elements of an array in ascending order\n";
     cin >> n;
 
+    for (int i = 0; i < n; i++)
+        cin >> data[i];
+
+    cout << "***** Given Array **********\n";
+    for (int i = 0; i < n; i++)
+        cout << data[i] << "\t";
+    cout << "\n";
+
+    cout << "Enter the element you are searching for\n";
+    cin >> item;
+
     
