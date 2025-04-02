d90dcac1fccb74b8a2715404a9d65602d9dfbd28
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-02 23:18:18 +0530

    insertion into BST

--- inserting_element_BST.cpp
+++ inserting_element_BST.cpp
@@ -2,4 +2,16 @@
 using namespace std;
 class BST
 {
-   
+    int data;
+    BST *left, *right;
+    public:
+        // Default constructor.
+        BST();
+        // Parameterized constructor.
+        BST(int);
+        // Insert function.
+        BST* Insert(BST *, int);
+        // Inorder traversal.
+        void Inorder(BST *);
+};
+
