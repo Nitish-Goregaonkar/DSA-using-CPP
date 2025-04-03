32b4932078b825672e60c827d837626ec9ead6c0
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-03 19:50:08 +0530

    shortest

--- shortestpath_algo_FWALGO.cpp
+++ shortestpath_algo_FWALGO.cpp
@@ -1,3 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include<bits/stdc++.h>
+// Number of vertices in the graph
+#define V 4
+/* Define Infinite as a large enough value.
+   This value will be used for vertices not connected to each other */
+#define INF 99999
 
