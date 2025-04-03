413f5ae1e6f6ae43985234d5f515030fbde9fa9c
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-03 19:51:49 +0530

    shortest

--- shortestpath_algo_FWALGO.cpp
+++ shortestpath_algo_FWALGO.cpp
@@ -40,4 +40,14 @@ void floydWarshall(int graph[][V]) {
 void printSolution(int dist[][V]) {
     cout << "Shortest distances between every pair of vertices:\n";
     for (int i = 0; i < V; i++) {
-        
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF "
+                     << " ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
