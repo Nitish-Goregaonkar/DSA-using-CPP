6f71695b1149cbc686c2405f133d57efcadd4612
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-04 22:04:44 +0530

    initial

--- shortestpath_algo_FWALGO.cpp
+++ shortestpath_algo_FWALGO.cpp
@@ -62,4 +62,9 @@ int main() {
        |     \|/
       (1)----->(2)
           3        */
+    int graph[V][V] = {{0, 5, INF, 10},
+                       {INF, 0, 3, INF},
+                       {INF, INF, 0, 1},
+                       {INF, INF, INF, 0}};
+
    
