c6ab4373aa8a80cd2f2ae3bc0f07b8e220b5b574
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-12 22:04:34 +0530

    dsa

--- binary.cpp
+++ binary.cpp
@@ -18,4 +18,12 @@ int binarysearch(int arr[], int n, int key) {
 int main() {
     int n;
     cin >> n;
-    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key;
+    cout << binarysearch(arr, n, key) << endl;
+    return 0;
+}
