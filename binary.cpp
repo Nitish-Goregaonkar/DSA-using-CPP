18eb90cb8c0dee1a3443d70d0d60bad09235a01a
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-12 22:01:46 +0530

    dsa

--- binary.cpp
+++ binary.cpp
@@ -4,4 +4,5 @@ int binarysearch(int arr[], int n, int key) {
     int s = 0;
     int e = n - 1;  // Corrected: 'e' should be n-1 instead of n
     while (s <= e) {
+        int mid = (s + e) / 2;
         
