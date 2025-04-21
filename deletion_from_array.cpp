17351d9a0aa98294f48633da6853e08be41ebe48
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-21 23:28:07 +0530

    new

--- deletion_from_array.cpp
+++ deletion_from_array.cpp
@@ -18,14 +18,4 @@ int main() {
         cout << "Invalid position! Please enter a position between 1 and " << n << ".\n";
         return 1; // Exit with an error code
     }
-    item = LA[k - 1];  // Store the element to be deleted
-    // Corrected the loop syntax
-    for (j = k - 1; j <= n - 2; j++) 
-        LA[j] = LA[j + 1];
-    n = n - 1; // Reduce the array size
-    cout << "*****Array after deletion********\n";
-    for (j = 0; j < n; j++)
-        cout << LA[j] << "\t";
-    cout << "\n";
-    return 0;
-}
+    
