d1a7194c634fa3dc28b71f3b9549545707a86ed8
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-23 22:46:18 +0530

    UPDATION

--- use_of_multidimensionar_array.cpp
+++ use_of_multidimensionar_array.cpp
@@ -63,4 +63,31 @@ Matrix matrixMultiplication(const Matrix &m1, const Matrix &m2)
         for (int j = 0; j < m3.getCol(); j++)
         {
             m3.setElement(i, j, 0); // Initialize to 0
-          
+            for (int k = 0; k < m1.getCol(); k++)
+            {
+                m3.ptrToMatrix[i][j] += m1.ptrToMatrix[i][k] * m2.ptrToMatrix[k][j];
+            }
+        }
+    }
+    return m3;
+}
+
+int main() // Main function
+{
+    int i, j, r, c, value;
+
+    // Input first matrix
+    cout << "Enter the dimensions of the first matrix (rows and columns): ";
+    cin >> r >> c;
+    Matrix mat1(r, c);
+    cout << "Enter the elements of the first matrix row by row:\n";
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            cin >> value;
+            mat1.setElement(i, j, value);
+        }
+    }
+
+    
